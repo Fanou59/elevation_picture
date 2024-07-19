7ad6edc04107cefc6e3a7769f18c46790f7a6d12
Ste[PH]ane <steph.guery@gmail.com>
2024-07-19 17:26:53 +0200

    init project

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center m-auto max-w-4 lg:flex-col gap-8 min-h-full">
+      <div>Card</div>
+      <div>Preview</div>
+    </main>
+  );
+}
